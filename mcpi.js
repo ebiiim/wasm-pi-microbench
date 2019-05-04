71541b29bf6d15736ec5192400ee393ba972edd9
ebiiim <mail@ebiiim.com>
2019-05-05 02:25:53 +0900

    chore: code formatting

--- mcpi.js
+++ mcpi.js
@@ -1,12 +1,11 @@
-
-function mcpi(loop){
+function mcpi(loop) {
     let c = 0;
-    for (let i=0; i<loop; i++){
+    for (let i = 0; i < loop; i++) {
         const x = Math.random();
         const y = Math.random();
-        if (x*x + y*y <= 1) c++;
+        if (x * x + y * y <= 1) c++;
     }
-    return c/loop*4.0
+    return c / loop * 4.0
 }
 
 function calcMcpi() {
@@ -15,8 +14,8 @@ function calcMcpi() {
     const pi = mcpi(iter);
     const end = new Date();
     const elapsed = end.getTime() - start.getTime();
-    const log1 = 'elapsed time: '+elapsed+' ms';
-    const log2 = 'iterations: '+iter+', estimated value: '+pi;
-    console.log(log1+'\n'+log2);
-    document.getElementById('resultJS').innerHTML = log1+'<br>'+log2;
+    const log1 = 'elapsed time: ' + elapsed + ' ms';
+    const log2 = 'iterations: ' + iter + ', estimated value: ' + pi;
+    console.log(log1 + '\n' + log2);
+    document.getElementById('resultJS').innerHTML = log1 + '<br>' + log2;
 }
